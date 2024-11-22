ca210f796c1ba7323c1c496b6ecf9e29d5b264ba
katerina mazi <katerina.aliki@hotmail.com>
2024-11-22 15:23:37 +0200

    Fix breaking logout

--- Our-Event-Server/OurEventApp/authentication.py
+++ Our-Event-Server/OurEventApp/authentication.py
@@ -1,8 +1,15 @@
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.models import Token
+
 
 class CookieTokenAuthentication(TokenAuthentication):
     def authenticate(self, request):
         token = request.COOKIES.get('auth_token')
         if not token:
             return None
+        
+        token_model = Token.objects.filter(key=token)
+        if not token_model.exists():
+            return None
+
         return self.authenticate_credentials(token)
